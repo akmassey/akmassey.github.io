2d71d2a8bf4a246809d69e6b7efa01bfc6ef0726
Aaron Massey <akmassey@ncsu.edu>
2011-06-29 16:22:12 -0400

    Adding Compass for styling

---
+++ compass.rb
@@ -0,0 +1,10 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "_site/css"
+sass_dir = "_sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+output_style = :compact
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
